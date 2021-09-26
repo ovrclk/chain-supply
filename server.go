32d884d2cfc48f4268502aa74b9e95b96d6c227d
Adam Bozanich <adam.boz@gmail.com>
2021-09-25 20:52:21 -0700

    add text endpoints

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
@@ -39,9 +40,45 @@ func (c *serverCmd) Run(ctx *runctx) error {
 
 	r.HandleFunc("/", showSummary)
 
+	r.HandleFunc("/circulating", func(w http.ResponseWriter, r *http.Request) {
+		status, err := getStatus(r.Context(), ctx.cctx, ctx.denom, ctx.locked)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Add("Content-Type", "text/plain")
+		fmt.Fprint(w, formatAmount(status.Circulating.Amount))
+	})
+
+	r.HandleFunc("/total", func(w http.ResponseWriter, r *http.Request) {
+		status, err := getStatus(r.Context(), ctx.cctx, ctx.denom, ctx.locked)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Add("Content-Type", "text/plain")
+		fmt.Fprint(w, formatAmount(status.Total.Amount))
+	})
+
+	r.HandleFunc("/bonded", func(w http.ResponseWriter, r *http.Request) {
+		status, err := getStatus(r.Context(), ctx.cctx, ctx.denom, ctx.locked)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Add("Content-Type", "text/plain")
+		fmt.Fprint(w, formatAmount(status.Bonded.Amount))
+	})
+
 	server := handlers.LoggingHandler(os.Stdout, r)
 
 	fmt.Printf("running server on port %v\n\n", c.Port)
 
 	return http.ListenAndServe(c.Port, server)
+}
+
+func formatAmount(amount sdk.Int) string {
+	whole := amount.QuoRaw(1000000).Uint64()
+	frac := amount.ModRaw(1000000).Uint64()
+	return fmt.Sprintf("%d.%06d", whole, frac)
 }
